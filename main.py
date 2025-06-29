a2db73039d1fa88fffdd8c9eee4f4477cdc0d6a6
Lxg1c <teran_2005@mail.ru>
2025-06-29 14:03:58 +0300

    add logger and admin routers

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import asyncio
-from models.dialog import Base
+import sys
+from models.base import Base
+from models.user import User
+from models.dialog import Dialog
 from config.config import token
-from handlers import user, operator
+from handlers import user, operator, admin
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from utils.middlewares_db import DbSessionMiddleware
+import logging
 
 
 engine = create_engine("sqlite:///data/database.db")
@@ -19,6 +23,7 @@ dp = Dispatcher(storage=MemoryStorage())
 dp.message.middleware(DbSessionMiddleware(SessionLocal))
 dp.callback_query.middleware(DbSessionMiddleware(SessionLocal))
 
+dp.include_router(admin.router)
 dp.include_router(user.router)
 dp.include_router(operator.router)
 
@@ -27,8 +32,9 @@ def print_startup():
     print(" ✅ Бот успешно запущен!")
     print("=" * 50 + "\n")
 
-async def main():
+async def main() -> None:
     print_startup()
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
