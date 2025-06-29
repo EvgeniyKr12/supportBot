1418ad08d9fc955e5f71aff2a53c58f98fc341b3
Lxg1c <teran_2005@mail.ru>
2025-06-29 17:13:25 +0300

    add super admin router

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from models.base import Base
 from models.user import User
 from models.dialog import Dialog
 from config.config import token
-from handlers import user, operator, admin
+from handlers import user, operator, admin, super_admin
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from aiogram import Bot, Dispatcher
@@ -23,19 +23,24 @@ dp = Dispatcher(storage=MemoryStorage())
 dp.message.middleware(DbSessionMiddleware(SessionLocal))
 dp.callback_query.middleware(DbSessionMiddleware(SessionLocal))
 
+
+dp.include_router(super_admin.router)
 dp.include_router(admin.router)
 dp.include_router(user.router)
 dp.include_router(operator.router)
+
 
 def print_startup():
     print("\n" + "=" * 50)
     print(" ✅ Бот успешно запущен!")
     print("=" * 50 + "\n")
 
+
 async def main() -> None:
     print_startup()
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
