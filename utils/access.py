586cb4afbbf26b6efe3f34ecaff5d25ba64edb0c
Lxg1c <teran_2005@mail.ru>
2025-07-05 21:17:48 +0300

    refactored

--- utils/access.py
+++ utils/access.py
@@ -7,10 +7,6 @@ from models import UserRole
 from services import UserService
 
 
-def is_admin(user):
-    return user and user.role in (UserRole.ADMIN, UserRole.SUPER_ADMIN)
-
-
 async def check_super_admin_access(update: Union[CallbackQuery, Message], db: Session):
     """Проверка прав супер-администратора"""
     user_service = UserService(db)
@@ -29,7 +25,7 @@ async def check_admin_access(update: Union[CallbackQuery, Message], db: Session)
     """Проверка прав администратора"""
     user_service = UserService(db)
     user = user_service.get_user(update.from_user.id)
-    if not is_admin(user):
+    if not user and user.role in (UserRole.ADMIN, UserRole.SUPER_ADMIN):
         if isinstance(update, CallbackQuery):
             await update.answer("❌ У вас нет доступа.")
             await update.message.answer("❌ У вас нет доступа.")
